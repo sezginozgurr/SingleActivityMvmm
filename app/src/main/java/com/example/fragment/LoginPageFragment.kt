d32741b49b9eef96991e082d2f82ac3729c18ee3
ozgurcan SEZGIN <sezginzgurr@gmail.com>
2020-08-04 16:29:01 +0300

    third Code

--- app/src/main/java/com/example/fragment/LoginPageFragment.kt
+++ app/src/main/java/com/example/fragment/LoginPageFragment.kt
@@ -1,21 +1,23 @@
 package com.example.fragment
 
-import android.os.Bundle
-import android.view.LayoutInflater
 import android.view.View
-import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.navigation.NavController
 import com.example.singeactivitymvvm.R
 
-class LoginPageFragment : Fragment() {
+class LoginPageFragment : Fragment(R.layout.fragment_login_page), View.OnClickListener {
 
+    var navController: NavController? = null
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_login_page, container, false)
+    override fun onClick(v: View?) {
+        when (v?.id) {
+            R.id.giris -> {
+                navController?.navigate(R.id.action_loginPageFragment_to_homePageFragment)
+            }
+            R.id.register -> {
+                navController?.navigate(R.id.action_loginPageFragment_to_registerFragment)
+            }
+        }
     }
 
 
--- app/src/main/java/com/example/fragment/RegisterFragment.kt
+++ app/src/main/java/com/example/fragment/RegisterFragment.kt
@@ -5,16 +5,25 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.navigation.NavController
 import com.example.singeactivitymvvm.R
 
-class RegisterFragment : Fragment() {
+class RegisterFragment : Fragment(), View.OnClickListener {
 
-
+    var navController: NavController? = null
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_register, container, false)
+    }
+
+    override fun onClick(v: View?) {
+        when (v?.id) {
+            R.id.rgstd_btn -> {
+                navController?.navigate(R.id.action_homePageFragment_to_detailFragment)
+            }
+        }
     }
 }
