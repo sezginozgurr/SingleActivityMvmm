c86389650a4b7978a2513f33a3745f99ef8e2847
ozgurcan SEZGIN <sezginzgurr@gmail.com>
2020-08-04 16:54:06 +0300

    added to button

--- app/src/main/java/com/example/fragment/LoginPageFragment.kt
+++ app/src/main/java/com/example/fragment/LoginPageFragment.kt
@@ -1,15 +1,36 @@
 package com.example.fragment
 
+import android.os.Bundle
+import android.view.LayoutInflater
 import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
 import androidx.fragment.app.Fragment
 import androidx.navigation.NavController
+import androidx.navigation.Navigation
 import com.example.singeactivitymvvm.R
 
-class LoginPageFragment : Fragment(R.layout.fragment_login_page), View.OnClickListener {
+class LoginPageFragment : Fragment(), View.OnClickListener {
 
     var navController: NavController? = null
 
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_login_page, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        navController = Navigation.findNavController(view)
+        view.findViewById<Button>(R.id.giris).setOnClickListener(this)
+        view.findViewById<Button>(R.id.register).setOnClickListener(this)
+    }
+
     override fun onClick(v: View?) {
+
         when (v?.id) {
             R.id.giris -> {
                 navController?.navigate(R.id.action_loginPageFragment_to_homePageFragment)
